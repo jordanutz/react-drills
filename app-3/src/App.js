57c22cfc70f96a07d06ee86106f658898ff8a522
jordanutz <jordanutz@outlook.com>
2019-01-13 22:06:56 -0700

    Completed React Drill 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,40 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      people: ['sam', 'sally', 'samantha', 'sidney', 'jason', 'salamendar'],
+      input: ''
+    }
+  }
+
+  handleInput = (event) => {
+    this.setState({
+      input: event.target.value.toLowerCase()
+    })
+  }
+
+
   render() {
+
+
+    let filteredFriends = this.state.people.filter( (person, index) => {
+      return person.includes(this.state.input)
+    }).map( (person, index) => {
+      return (
+        <h2 key={index}>{person}</h2>
+      )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>React Drill 3</h1>
+        {filteredFriends}
+
+        <h2>Filter Here</h2>
+
+        <input onChange={this.handleInput}/>
       </div>
     );
   }
