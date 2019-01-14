638427720303884421ac8ea2fa63923510696312
jordanutz <jordanutz@outlook.com>
2019-01-13 21:29:58 -0700

    Completed React 2 Drill

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -3,16 +3,28 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      roomies: ['Spencer', 'Tyler', 'Prakash', 'Tyler', 'Elliot']
+    }
+  }
+
+
   render() {
+
+    let displayedRoomies = this.state.roomies.map( (roomie, index) => {
+      return (
+        <div className="roommies" key={index}>
+          {roomie}
+        </div>
+      )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>My List</h1>
+        {displayedRoomies}
       </div>
     );
   }
